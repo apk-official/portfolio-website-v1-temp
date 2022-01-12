c7f5326a6ada82ae241f09b614adb8b8cc8b1e1c
Abhinav P Kumar <abhinavpkumar@Abhinavs-MacBook-Air.local>
2022-01-12 18:25:32 +0530

    Portfolio Section added 😊

--- Resources/Js/navigation.js
+++ Resources/Js/navigation.js
@@ -1,10 +1,14 @@
 const home = document.querySelector(".home");
 const about = document.querySelector(".about");
+const portfolio = document.querySelector(".portfolio");
 const skill = document.querySelector(".skill");
 const contact = document.querySelector(".contact");
 
 const navAbout = about.addEventListener("click", () => {
   window.location.href = "../About/";
+});
+const navPortfolio = portfolio.addEventListener("click", () => {
+  window.location.href = "../Portfolio/";
 });
 const navHome = home.addEventListener("click", () => {
   window.location.href = "../";
